ce3d9c997ea9a9d4c3c87572d0f91a9eae6b77b4
TauWoo <tauwoo@seuxw.cc>
2018-03-05 18:03:26 +0800

    网页日志模块更新

--- handler/main.py
+++ handler/main.py
@@ -1,4 +1,35 @@
 from flask import Flask, Blueprint
+from util.common.logger import use_logger
+
+@use_logger(level="info")
+def web_info(msg):
+    pass
+
+@use_logger(level="warn")
+def web_warn(msg):
+    pass
+
+@use_logger(level="err")
+def web_err(msg):
+    pass
+
+@use_logger(level="fatal")
+def web_fatal(msg):
+    pass
+
+
+def web_logger(level, funcname, inpara="", out=""):
+    msg = "{funcname}\tIn:{inpara}\tReturn:{out}".format(funcname=\
+    funcname, inpara=inpara, out=out)
+
+    if level == "info":
+        web_info(msg)
+    elif level == "warn":
+        web_warn(msg)
+    elif level == "err":
+        web_err(msg)
+    elif level == "fatal":
+        web_fatal(msg)
 
 # flaskapp的基础配置
 app = Flask(__name__)
@@ -9,3 +40,4 @@ app.jinja_env.variable_end_string = " }}"
 bp = Blueprint('flask', __name__, subdomain='flask')
 app.register_blueprint(bp)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+web_logger("info",__name__)
--- handler/test.py
+++ handler/test.py
@@ -1,8 +1,8 @@
 import json, requests
 from flask import request, render_template
-from .main import app
+from .main import app, web_logger
 
-@app.route("/test", methods=['GET'])
+@app.route(rule="/test", methods=['GET'])
 def test():
     '''
     TestPage
@@ -11,21 +11,25 @@ def test():
     '''
     date = request.args.get("date")
 
-    if date is None:
-        return render_template('test.html', sucess=False, message="请携带参数date")
-
     test_url = "http://remote.seuxw.cn/test?date={date}".format(date=date)
     req_content = json.loads(str(requests.get(test_url).text))
 
     success = False
     message = str()
 
+    if date is None:
+        message="请携带参数date"
+        web_logger("info", __name__, """{"date":None}""", message)
+        return render_template('test.html', sucess=False, message=message)
+
     if req_content["code"] == 0:
         success = True
-        message = "日期：{date}\n日出时间：{time_rise}\n日落时间：{time_down}".format(date=date, \
+        message = "日期：{date} - 日出时间：{time_rise} - 日落时间：{time_down}".format(date=date, \
         time_rise=req_content["data"]["sun_rise_time"],time_down=req_content["data"]["sun_down_time"])
     else:
         success = False
         message = req_content["message"]
+
+    web_logger("info", __name__, """{"date":"%s"}"""%date, message)
     
     return render_template('test.html', success=success, message=message)
