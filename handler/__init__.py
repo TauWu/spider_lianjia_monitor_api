85f5fc9c273591b7fe01f5eea4fa0ce00720190f
TauWoo <tauwoo@seuxw.cc>
2018-03-05 17:32:30 +0800

    Handler打包

--- handler/__init__.py
+++ handler/__init__.py
@@ -1,4 +1,2 @@
 from .main import app
 from .test import test
-
-__all__ = ['Flask', 'request', 'render_template', 'jsonify', 'g', 'Blueprint', 'session', 'redirect', 'url_for', 'requests', 'json', 'app']
